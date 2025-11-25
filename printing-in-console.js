ba88fc5414237bcb48130ca4b75eace6ae0aec1b
dark-er-00 <javillanarolandcarl@gmail.com>
2025-11-25 09:45:32 +0800

    module 1

---
+++ printing-in-console.js
@@ -0,0 +1,12 @@
+console.log("Roland Carl Javillana");
+
+console.log("2006");
+
+console.log(2006);
+
+console.log("Roland Carl Javillana (2006)");
+console.log("Roland Carl", "Javillana", "(2006)");
+
+console.log("Roland Carl\nJavillana\n(2006)");
+
+console.log("ROland Carl" + " Javillana" + " (2006)")
